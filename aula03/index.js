b165a7e48f4d80158331ca4e95a93fefd4ea12ab
TulioBreyner <marcopbreyner@gmail.com>
2025-08-28 19:58:34 -0300

    aula04 (28/09/2025)
    continuação da aula03

--- aula03/index.js
+++ aula03/index.js
@@ -14,11 +14,27 @@ conectaDB(); // conexão com o MongoDB
 
 app.get('/', (req, res) => {
     res.json({message: "hello world"});
-})
+});
 
 // rotas
 app.use('/alunos', alunosRouter);
 
+// Handler de erros
+app.use((err, req, res, next) => {
+    console.error(`Erro: ${err}`);
+    // CastError
+    if (err.name === "CastError"){
+        return res.status(400).json({erro: "ID inválido"});
+    }
+
+    // Erro de validação
+    if (err.name === "ValidationError"){
+        return res.status(400).json({erro: "Validação falhou", detalhes: err.errors});
+    }
+
+    res.status(500).json({erro: "Erro interno do servidor"});
+});
+
 app.listen(port, () => {
     console.log(`Servidor na porta ${port}`)
-})
+});
